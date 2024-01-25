c4f474bdade610655eb428b9b71ee58d1abca555
Ameur mohamed ayoub <2703320@collegelacite.ca>
2024-01-25 00:10:49 -0500

    HTML Markup
    HTML Markup with style

---
+++ guess.js
@@ -0,0 +1,4 @@
+let gameName ="Guess The word";
+document.title=gameName;
+document.querySelector("h1").innerHTML=gameName;
+document.querySelector("footer").innerHTML=`${gameName} &copy; 2024 MedAmeur. All rights reserved.`
